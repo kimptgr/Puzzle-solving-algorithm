36a947f0550f3f8c3d45b76b479f78562d93a9d3
kimptgr <patingrekim@gmail.com>
2025-07-01 18:03:37 +0200

    first commit

---
+++ helpful_server.py
@@ -0,0 +1,38 @@
+FERN = "🎍"
+DISH = "🍝"
+GIRL_COL = "🔽"
+GIRL_RAW = "◀️"
+BOY_RAW = "▶️"
+BOY_COL = "🔼"
+
+elements = [GIRL_COL, GIRL_RAW, BOY_COL, BOY_RAW, DISH]
+
+
+def solve():
+    pass
+
+
+def next_item():
+    pass
+
+
+def find_best_item():
+    pass
+
+
+def is_correct():
+    pass
+
+
+def count_choices(puzzle: list[list[str]], case) -> int:
+    choices = 0
+
+    return choices
+
+
+def choose_puzzle():
+    pass
+
+
+def print_party():
+    pass
